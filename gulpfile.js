c7fd5d1560a780d4be117661327cc30d4560f1e2
yhabib <habibfernandez@gmail.com>
2017-09-23 11:26:45 +0200

    Typo v.2

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('default', () => console.log("Gulp is working"));
 //// TESTS
 //////////////////////////////////////////////////
 
-gulp.task('test_data_structures', () => {
+gulp.task('test-data-structures', () => {
   return gulp.src('./data-structures/**/*.spec.js', { read: false })
     .pipe(mocha({ reporter: 'list' }))
     .on('error', gutil.log);
@@ -20,6 +20,6 @@ gulp.task('test_data_structures', () => {
 //// WATCHERS
 //////////////////////////////////////////////////
 
-gulp.task('tdd_ds', ['test_data_structures'], () => {
+gulp.task('tdd_ds', ['test-data-structures'], () => {
   gulp.watch(['./data-structures/**/*.js', './data-structures/**/*.spec.js'], ['test_data_structures']);
 });
